542b2c3162a71b1df6a3769def1effef71faa512
Jack Boberg <jack.boberg@gmail.com>
2011-01-07 23:11:31 -0500

    removed old code, got basic formatting of output tested

--- js/mylibs/cbh_shell.js
+++ js/mylibs/cbh_shell.js
@@ -46,286 +46,94 @@ function cbh_shell_parser(shell){
         };
         return that.opts['output'];
     };
+    
+    this.clear = function (){
+        this.program = '';
+        this.opts = {};
+    };
 }
 
-function cbh_shell(){
+function cbh_shell(config){
     var that = this,
         cmd_queue = [],
-        output = []//,
+        output = [],
         // input_str = '',
         input_chars = []
         ;
+
+    var defaults = {
+        autoload_dir: '/js/shell_programs'
+    };
+    if (config) {
+        for (attrname in defaults) {
+            defaults[attrname] = config[attrname];
+        };
+    };
 
     this.program = '';
     this.input_chars = [];
 
     this.parser = new cbh_shell_parser(this);
     
-    this.run = function(line){
+    this.run = function(line){        
         that.input_chars = line.split('');
         
         var prog = that.parser.get_program();
+        
+        var messages = [];
         if ( ! prog) {
-            return [
-            {
+            messages.push({
                 msg: 'Program not found',
-                className: "jquery-console-message-type"
-            }];
+                className: "jquery-console-message-error"
+            });
+        } else {
+            messages.push({
+                msg: 'Program: ' + prog,
+                className: "jquery-console-message-value"
+            });
         }
         
         var opts = that.parser.get_options();
         if ( ! opts) {
-            return [
-            {
-                msg: 'Opts not found',
-                className: "jquery-console-message-type"
-            }];
-        };
-        console.log(opts);
-        return [
-        {
-            msg: 'End of the line.',
+            messages.push({
+                msg: 'No options passed.',
+                className: "jquery-console-message-error"
+            });
+        } else {
+            messages.push({
+                msg: 'Opts: ' + opts + ' (not parsed yet)',
+                className: "jquery-console-message-value"
+            });
+        }
+        messages.push({
+            msg: 'done processing',
             className: "jquery-console-message-type"
-        }];
-        
-        // return [
-        // {
-        //   msg: 'input: ' + line,
-        //   className: "jquery-console-message-type"
-        // },
-        // {
-        //   msg: "program: " + p.program,
-        //   className: "jquery-console-message-value"
-        // }
-        // ];
-    };
-
-    function set_program () {
-        var cur_char;        
-        while (that.input_chars) {
-            cur_char = that.input_chars.shift();
-            if (cur_char == ' ') return;
-            that.program += cur_char;
-        };
-    }
-    
-    function parse_options () {
-        var cur_char;        
-        while (that.input_chars) {
-            cur_char = that.input_chars.shift();
-            switch (cur_char){
-                case '-':
-                    console.log('dash');
-                    break;
-                default:
-                    console.log(cur_char);
-            };         
-        };
-    };
-    
-    this.parse_input = function(input_str){
-        console.log('input_str:',input_str);
-        
-        this.input_chars = input_str.split('');        
-        console.log('input_chars:',this.input_chars);
-        
-        set_program();
-        console.log('program',this.program);
-        console.log('input_chars:',this.input_chars);
-        
-        if (this.input_chars) {
-            console.log('input_chars:',this.input_chars);
-            // parse_options();
-        };
-        
-        return [{
-          msg: this.program,
-          className: "jquery-console-message-value"
-        }];
-        
-    };
-
-    //     var prog,
-    //         prog_opts={},
-    //         cur_opt={
-    //             key:'',
-    //             val:''
-    //         },
-    
-    /*
-
-    first set the program name
-    record all chars
-    when you hit the first space start looking for options (ignore the space)
-        if you found a dash start a new opt (ignore the dash)
-            if you find another dash
-                the batch of charachters is a KEY string
-            if you find another char
-                it is a new opt with a single char KEY
-
-            - KEY is default to true, but could be set other wise
-
-            the next space begins looking for the next opt
-
-
-    */
-
-    //     
-    //     // for (var i=0; i < chars.length; i++) {
-    //     // };
-    //     // seperate commands
-    //     // var commands = input_str.split(';'),
-    //     //     params, cmd, bits;
-    //     // for (var i=0, j=commands.length; i < j; i++) {
-    //     //     // seperate command from args
-    //     //     bits = $.trim(commands[i]).split(' ');
-    //     //     cmd = bits.shift();
-    //     //     if (cmd) {
-    //     //         cmd_queue.push({
-    //     //             'params': parse_params_str(bits),
-    //     //             'cmd': cmd
-    //     //         });
-    //     //     };            
-    //     // };
-    //     // return execute();
-    // };
-    
-    function execute () {
-        var cur_cmd = cmd_queue.shift(),
-            message = cur_cmd.cmd + ': ';
-        
-        for (var opt in cur_cmd.params) {
-            message += opt + '('+cur_cmd.params[opt]+');';
-        };
-        output.push({
-          msg: message,
-          className: "jquery-console-message-value"
         });
-        
-        if (cmd_queue.length) {
-            return execute();
-        };
-        var messages = output;
-        output = [];
+        that.parser.clear();
         return messages;
     };
     
+    // function execute () {
+    //     var cur_cmd = cmd_queue.shift(),
+    //         message = cur_cmd.cmd + ': ';
+    //     
+    //     for (var opt in cur_cmd.params) {
+    //         message += opt + '('+cur_cmd.params[opt]+');';
+    //     };
+    //     output.push({
+    //       msg: message,
+    //       className: "jquery-console-message-value"
+    //     });
+    //     
+    //     if (cmd_queue.length) {
+    //         return execute();
+    //     };
+    //     var messages = output;
+    //     output = [];
+    //     return messages;
+    // };
+    
 };
 
-
-//------------------------------------------
-//  OLD STUFF
-
-/*
-function cbh_terminal(input_el, history_el) {
-	// TODO: protect for input_el and history_el not passed
-
-	var that = this,
-		history_line_el = {
-			tag: 'li',
-			attrs: {}
-		};
-
-	this.commands = {};	
-	// var programs = {};
-
-	// event listener for submitted input
-	input_el.keyup(
-		[this],
-		function(e){
-			if (e.keyCode != '13') return;
-			e.preventDefault();
-        	
-			var cmd = $(this).val();
-			$(this).val(''); // clear input
-
-			var term = e.data[0];
-			term.cli(cmd);
-		}
-	);
-	
-	function error(err_str){
-		that.puts(err_str);
-		return that;
-	};
-
-	// ----------------------------------------------------------
-	// Public Methods
-	
-	this.puts = function (str){
-		// TODO: parse into lines and append
-		history_line_el.attrs.text = str;
-		$('<'+history_line_el.tag+'>', history_line_el.attrs)
-			.appendTo(history_el);
-		return this;
-	};	
-
-	this.add = function(cmd, obj){
-		// TODO: protect for overwriting?
-		this.commands[cmd] = obj;
-		if (obj.alias) {
-			for (var i=0, j=obj.alias.length; i < j; i++) {
-				this.commands[obj.alias[i]] = this.commands[cmd];	
-			};
-		};
-	};
-
-	this.alias = function (shortcut, cmd) {
-		// TODO
-		console.log('alias:', shortcut, cmd);
-	};
-
-	// TODO: this is a terrible function
-	// needs to be refactored into parse_input()
-	// so that you could 'manually' run commands without using
-	// input parser
-	
-	this.cli = function(input){		
-		// TODO:
-		// this is weak sauce, and needs to get awesome regex smart
-		// and move to a parse_input function
-		var params = input.split(' ');
-		var cmd = params.shift();
-		
-		if ( ! this.commands[cmd]) {
-			return error("'"+cmd+"' not found");
-		}
-		
-		if ( ! this.commands[cmd].run) {
-			return error("Unable to run '"+cmd+"'");
-		};
-
-		return this.commands[cmd].run(params);
-		
-		// what about something like
-		// cmd --params > callback
-		// this.callback = callback
-		// if (this.callback) { this.callback(this.output);}
-		
-	};
-	
-	// ----------------------------------------------------------
-	// Create default programs
-
-	this.add('help', {
-		alias: ['?'],
-		run: function(params){
-			var output = 'This is the default help text';
-
-			if (params.length) {
-				var cmd = params[0];
-				if ( ! that.commands[cmd] || ! that.commands[cmd].help) {
-					output = "No help found for '"+cmd+"'";
-				} else {
-					return that.commands[cmd].help();
-				};
-			};
-
-			return that.puts(output);
-		} 
-	});
-	
-}
-*/
 
 
--- js/script.js
+++ js/script.js
@@ -3,12 +3,12 @@
 */
 $(document).ready(function() {
 
-    /* First console */
+    /* Create console */
     var cbh_term = $('<div class="console">');
     $('body').append(cbh_term);
     var controller = cbh_term.console({
         promptLabel: 'cbh> ',
-        // TODO: user@cbh>
+        // TODO:? $user_name@cbh>
         commandValidate: function(line) {
             if (line == "") return false;
             else return true;
@@ -32,49 +32,21 @@ $(document).ready(function() {
     };
     
     var cli = new cbh_shell();
-    
+    // 
 
     // show/hide with hotkey
-    // cbh_term.hide('blind');
     $(document)
-    .bind('click',
-    function() {
-        if (cbh_term.is(':visible')) {
+        .bind('click', function() {
+            if (cbh_term.is(':visible')) {
+                cbh_term.toggle('blind');
+            };
+        })
+        .bind('keydown', 'ctrl+t', function() {
             cbh_term.toggle('blind');
-        };
-    })
-    .bind('keydown', 'ctrl+t',
-    function() {
-        cbh_term.toggle('blind');
-        if (cbh_term.is(':visible')) {
-            cbh_term.click();
-        } else {
-            $(document).click();
-        }
-    });
-
-    // 	var cli_input	= $('#se_cli_input'),
-    // 		cli_history	= $('#se_cli_history');
-    // 	
-    // 	var term = new cbh_terminal(cli_input, cli_history);
-    // 	
-    // 	
-    // 	term.add('foo', {
-    // 		alias: ['bar'],
-    // 		run: function(params){
-    // 			var output = 'I am foo!';
-    // 			for (var i=0, j=params.length; i < j; i++) {
-    // 				output += ' '+params[i];
-    // 			};
-    // 			return term.puts(output);
-    // 		},
-    // 		help: function(){
-    // 			var output = 'I have my own help text.';
-    // 			return term.puts(output);
-    // 		}
-    // 	});
-
-
-
-
+            if (cbh_term.is(':visible')) {
+                cbh_term.click();
+            } else {
+                $(document).click();
+            }
+        });
 });
